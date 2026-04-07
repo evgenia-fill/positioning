61ffb3e01ccf8d83a3491a567caedafe54d00a78
MDmitriy74 <dmostovov74@gmail.com>
2026-04-07 19:19:05 +0500

    make task number 2

--- index.js
+++ index.js
@@ -5,3 +5,25 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const overlay = document.getElementById('modalOverlay');
+
+function openModal() {
+    overlay.classList.add('active');
+}
+
+function closeModal() {
+    overlay.classList.remove('active');
+}
+
+window.onclick = function(event) {
+    if (event.target == overlay) {
+        closeModal();
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === "Escape") {
+        closeModal();
+    }
+});
